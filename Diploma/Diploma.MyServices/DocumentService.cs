449c40fe968770a51771ec1809e8634cb0dadf51
Siarhei Opolko <Siarhei_Opolko@epam.com>
2017-04-13 22:48:59 +0300

    Update document service

--- Diploma/Diploma.MyServices/DocumentService.cs
+++ Diploma/Diploma.MyServices/DocumentService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Diploma.Core.Models;
 using Diploma.Repositories;
@@ -21,8 +22,6 @@ namespace Diploma.Services
 
             result.FileDownloadName = CreateDocumentNameUsingVersion(document);
 
-            result.FileDownloadName = document.DocumentName;
-
             return result;
         }
 
@@ -33,13 +32,15 @@ namespace Diploma.Services
 
         public async Task Save(IFormFile file, ApplicationUser user)
         {
+            var documentName = ParseFileName(file.FileName);
+
             var document = new Document
             {
                 ContentType = file.ContentType,
-                DocumentName = file.FileName,
+                DocumentName = documentName,
                 ApplicationUser = user,
                 UploadedDate = DateTime.Now,
-                Version = "1",
+                Version = await SetDocumentVersion(documentName),
                 DocumentAccesses = new List<DocumentAccess>
                 {
                     new DocumentAccess
@@ -57,9 +58,56 @@ namespace Diploma.Services
             await _documentRepository.Save(document, user);
         }
 
+        private string ParseFileName(string fileName)
+        {
+            var pointIndex = fileName.LastIndexOf('.');
+
+            var underscoreIndex = fileName.LastIndexOf('_');
+
+            var version = fileName.Substring(underscoreIndex, pointIndex - underscoreIndex);
+
+            return fileName.Replace(version, string.Empty);
+        }
+
+        private async Task<string> SetDocumentVersion(string fileFileName)
+        {
+            var documents = await _documentRepository.GetAll();
+
+            var existingDocuments = documents
+                .Where(x => x.DocumentName == fileFileName)
+                .ToList();
+
+            if (existingDocuments.Any())
+            {
+                var versions = existingDocuments.Select(x => int.Parse(x.Version))
+                    .OrderBy(i => i);
+
+                var lastLoadedVersion = versions.Last();
+
+                var version = lastLoadedVersion + 1;
+
+                return version.ToString();
+            }
+            else
+            {
+                return "1";
+            }
+        }
+
         private string CreateDocumentNameUsingVersion(Document document)
         {
-            return null;
+            if (string.IsNullOrEmpty(document.Version))
+            {
+                return document.DocumentName;
+            }
+            else
+            {
+                var index = document.DocumentName.LastIndexOf('.');
+            
+                var name = document.DocumentName.Insert(index, $"_{document.Version}");
+
+                return name;
+            }
         }
     }
 }
